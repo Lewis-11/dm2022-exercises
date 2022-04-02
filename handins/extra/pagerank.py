cfc204d3620b083bdc31b5950b1ae40b8b91f911
Lewis-11 <100383365@alumnos.uc3m.es>
2022-04-02 11:25:06 +0200

    checkpoint

--- handins/extra/pagerank.py
+++ handins/extra/pagerank.py
@@ -1,8 +1,20 @@
 import numpy as np
 
-adjacencyMatrix = [[0, 1, 0, 0], [1, 0, 1, 1], [0, 1, 0, 1], [0, 1, 1, 0]]
+adjacencyMatrix = [
+    [0, 1, 0, 0, 0],
+    [1, 0, 1, 1, 1],
+    [0, 1, 0, 1, 1],
+    [0, 1, 1, 0, 1],
+    [0, 1, 1, 1, 0],
+]
 
-outDegreeMatrix = [[1, 0, 0, 0], [0, 3, 0, 0], [0, 0, 2, 0], [0, 0, 0, 2]]
+outDegreeMatrix = [
+    [1, 0, 0, 0, 0],
+    [0, 4, 0, 0, 0],
+    [0, 0, 3, 0, 0],
+    [0, 0, 0, 3, 0],
+    [0, 0, 0, 0, 3],
+]
 
 n_nodes = len(adjacencyMatrix)
 
@@ -20,14 +32,23 @@ outDegreeMatrix = np.array(outDegreeMatrix)
 
 M = np.dot(adjacencyMatrix.T, np.linalg.inv(outDegreeMatrix))
 print(M)
-for i in range(30):
-    r.append(np.dot(M, r[-1]))
+print()
+# for i in range(30):
+#     r.append(np.dot(M, r[-1]))
 
 
-# Print r iteration by iteration
-for i in range(len(r)):
-    print(f"[i] Iteration {i}:")
+# # Print r iteration by iteration
+# for i in range(len(r)):
+#     print(f"[i] Iteration {i}:")
 
-    for j in range(len(r[i])):
-        print(f"{r[i][j]}\t", end="")
-    print("\n")
+#     for j in range(len(r[i])):
+#         print(f"{r[i][j]}\t", end="")
+#     print("\n")
+
+iterations = 30
+M_raised = np.linalg.matrix_power(M, iterations)
+final_result = np.dot(M_raised, r0)
+
+for i in range(1, 10):
+    print(np.linalg.matrix_power(M, i))
+    print()
